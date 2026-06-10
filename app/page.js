4e139fc2e30a1e5c588eb879f1d378aeabf2c8bc
Pickettlabs <pickettlabs@gmail.com>
2026-06-10 16:35:01 -0400

    Add files via upload

--- app/page.js
+++ app/page.js
@@ -15,7 +15,8 @@ export default function Page() {
         <div key={index} style={{ marginTop: 20 }}>
           <h3>{beat.title}</h3>
 
-            <audio controls src={beat.url}></audio>
+          {/* ✅ CORRECT AUDIO PLAYER */}
+          <audio controls src={beat.url}></audio>
 
           <p>${beat.price}</p>
 
