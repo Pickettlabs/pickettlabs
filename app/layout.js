e184b9e7d7416bfec137af6c06a50f0e13889de6
Pickettlabs <pickettlabs@gmail.com>
2026-07-08 11:53:23 -0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,13 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "Pickett Labs",
+  description: "Beat Store",
+};
+
 export default function RootLayout({ children }) {
   return (
-    <html>
+    <html lang="en">
       <body>{children}</body>
     </html>
   );
